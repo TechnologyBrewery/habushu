3a28c7aff3be2672baea48bbb49ea5b6738095aa
Andrew Walters <awalters@cpointe-inc.com>
2021-05-13 15:08:48 -0400

    HAB-27 multiple improvements for the clean lifecycle handling, including performance and ease of plugin use

--- habushu-maven-plugin/src/main/java/org/bitbucket/cpointe/habushu/HabushuCleanMojo.java
+++ habushu-maven-plugin/src/main/java/org/bitbucket/cpointe/habushu/HabushuCleanMojo.java
@@ -1,13 +1,15 @@
 package org.bitbucket.cpointe.habushu;
 
 import java.io.File;
+import java.lang.reflect.Field;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.clean.CleanMojo;
-import org.bitbucket.cpointe.habushu.util.HabushuUtil;
+import org.apache.maven.plugins.clean.Fileset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,18 +45,49 @@ public class HabushuCleanMojo extends CleanMojo {
 			if (!habushuForceClean) {
 				logger.debug(
 						"Habushu force clean not selected.  Sparing the /build-accelerator/ and /virtualenvs/ folders.");
+
+				Fileset buildDirectory = new Fileset();
+
+				String[] excludes = new String[2];
+				excludes[0] = "build-accelerator/**";
+				excludes[1] = "virtualenvs/**";
+
+				try {
+					FieldUtils.writeField(buildDirectory, "directory", workingDirectory, true);
+					FieldUtils.writeField(buildDirectory, "excludes", excludes, true);
+				} catch (IllegalAccessException e) {
+					throw new HabushuException("Could not write to private field in Fileset class.", e);
+				}
+
+				Fileset[] filesets = new Fileset[1];
+				filesets[0] = buildDirectory;
+
+				setPrivateParentField("excludeDefaultDirectories", true);
+				setPrivateParentField("filesets", filesets);
 			} else {
 				logger.debug(
 						"Habushu force clean selected.  Clearing away /build-accelerator/ and /virtualenvs/ folders.");
-
-				try {
-					HabushuUtil.changePrivateCleanMojoField(this.getClass().getSuperclass(), "excludeDefaultDirectories", false);
-				} catch (SecurityException e) {
-					throw new HabushuException("Could not access field in CleanMojo", e);
-				}
-
-				super.execute();
 			}
+
+			super.execute();
 		}
+	}
+
+	/**
+	 * Sets a given field in the CleanMojo with a provided value.
+	 * 
+	 * @param fieldName the name of the field in CleanMojo
+	 * @param value     the field's intended value
+	 */
+	private void setPrivateParentField(String fieldName, Object value) {
+		Field fieldInParentClass;
+		try {
+			fieldInParentClass = this.getClass().getSuperclass().getDeclaredField(fieldName);
+			fieldInParentClass.setAccessible(true);
+			fieldInParentClass.set(this, value);
+		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			throw new HabushuException("Could not write to field in CleanMojo class.", e);
+		}
+
 	}
 }
