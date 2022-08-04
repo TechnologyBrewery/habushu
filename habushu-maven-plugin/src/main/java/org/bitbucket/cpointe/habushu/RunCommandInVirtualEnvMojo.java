e584fc01bf2f1efa841215737ec4d4974ffeb832
Eric Konieczny <ekoniec1@gmail.com>
2022-08-04 14:15:52 +0000

    Merged in HAB-48 (pull request #51)
    HAB-48: Added mojo to support the execution of any command or script through "poetry run"
    
    * HAB-48: Added mojo to support the execution of any command or script through "poetry run"
    
    
    Approved-by: Aaron Gary
    Approved-by: Ryan Ashcraft

---
+++ habushu-maven-plugin/src/main/java/org/bitbucket/cpointe/habushu/RunCommandInVirtualEnvMojo.java
@@ -0,0 +1,43 @@
+package org.bitbucket.cpointe.habushu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.bitbucket.cpointe.habushu.exec.PoetryCommandHelper;
+
+/**
+ * Leverages {@code poetry run} to execute a Python command or script within
+ * this Poetry project's virtual environment. For example, this goal might be
+ * bound to the {@code compile} phase to facilitate the generation of
+ * gRPC/protobuf bindings as an automated part of the build following dependency
+ * installation.
+ */
+@Mojo(name = "run-command-in-virtual-env")
+public class RunCommandInVirtualEnvMojo extends AbstractHabushuMojo {
+
+    /**
+     * Whitespace-delimited command arguments that will be provided to
+     * {@code poetry run} to execute. For example, if {@code python -V} is provided
+     * to this parameter, {@code poetry run python -V} will be executed within the
+     * Poetry package's virtual environment.
+     */
+    @Parameter(property = "habushu.runCommandArgs")
+    protected String runCommandArgs;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+	PoetryCommandHelper poetryHelper = createPoetryCommandHelper();
+	List<String> poetryRunCommandArgs = new ArrayList<>(Arrays.asList(StringUtils.split(runCommandArgs)));
+	poetryRunCommandArgs.add(0, "run");
+
+	getLog().info("Executing command in virtual environment via 'poetry run'...");
+	poetryHelper.executeAndLogOutput(poetryRunCommandArgs);
+    }
+
+}
--- habushu-mixology/tests/features/steps/reference_src.py
+++ habushu-mixology/tests/features/steps/reference_src.py
@@ -1,11 +1,15 @@
 from behave import *
 from habushu_mixology.reusable_module.worker import SubWorker
 from habushu_mixology.helloworld import generate_random_string
+from habushu_mixology.generated import person_pb2
 
 
 @when("I reference a src file in my test file")
 def step_impl(context):
     context.random = generate_random_string(5)
+    person = person_pb2.Person()
+    person.email = "habushu@gmail.com"
+    context.person = person
 
 
 @when("I reference a src file that has references to other src files")
@@ -17,6 +21,7 @@ def step_impl(context):
 @then("the build can successfully execute the tests")
 def step_impl(context):
     assert len(context.random) == 5
+    assert context.person.email == "habushu@gmail.com"
 
 
 @then("the build can successfully resolve the imports")
