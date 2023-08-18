e0d95f832785c66d0c4cae8ccdf5a21a7f97df83
Ryan Ashcraft <ryan.ashcraft@codifyiq.com>
2023-08-18 17:09:49 -0400

    #30 :sparkles: incorporate Maven Reactor support; with PR feedback

--- habushu-maven-plugin/src/main/java/org/technologybrewery/habushu/BuildDeploymentArtifactsMojo.java
+++ habushu-maven-plugin/src/main/java/org/technologybrewery/habushu/BuildDeploymentArtifactsMojo.java
@@ -1,6 +1,8 @@
 package org.technologybrewery.habushu;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,68 +28,94 @@ import org.technologybrewery.habushu.exec.PoetryCommandHelper;
 public class BuildDeploymentArtifactsMojo extends AbstractHabushuMojo {
 
     /**
-     * By default, export requirements.txt file
+     * By default, export requirements.txt file.
      */
     @Parameter(property = "habushu.exportRequirementsFile", required = false, defaultValue = "true")
     protected boolean exportRequirementsFile;
 
     /**
-     * By default, do not include the --without-urls flag when exporting
+     * By default, do not include the --without-urls flag when exporting.
      */
     @Parameter(property = "habushu.exportRequirementsWithUrls", required = false, defaultValue = "true")
     protected boolean exportRequirementsWithUrls;
 
     /**
-     * By default, do not include the --without-hashes flag when exporting
+     * By default, do not include the --without-hashes flag when exporting.
      */
     @Parameter(property = "habushu.exportRequirementsWithHashes", required = false, defaultValue = "true")
     protected boolean exportRequirementsWithHashes;
 
     /**
-     * By default, export to the dist folder to be included with the build archive
+     * By default, export to the dist folder to be included with the build archive.
      */
     @Parameter(property = "habushu.exportRequirementsFolder", required = false, defaultValue = "${project.basedir}/dist")
     protected String exportRequirementsFolder;
 
+    /**
+     * Location of the artifact that will be published for this module.
+     */
+    @Parameter(property = "habushu.mavenArtifactFile", required = true, defaultValue = "${project.basedir}/target/habushu.placeholder.txt")
+    protected File mavenArtifactFile;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-	PoetryCommandHelper poetryHelper = createPoetryCommandHelper();
+        PoetryCommandHelper poetryHelper = createPoetryCommandHelper();
 
-	String buildCommand, buildLogMessage;
-	if (this.rewriteLocalPathDepsInArchives) {
-	    buildCommand = "build-rewrite-path-deps";
-	    buildLogMessage = "Building source and wheel archives with poetry-monorepo-dependency-plugin...";
-	} else {
-	    buildCommand = "build";
-	    buildLogMessage = "Building source and wheel archives...";
-	}
+        String buildCommand, buildLogMessage;
+        if (this.rewriteLocalPathDepsInArchives) {
+            buildCommand = "build-rewrite-path-deps";
+            buildLogMessage = "Building source and wheel archives with poetry-monorepo-dependency-plugin...";
+        } else {
+            buildCommand = "build";
+            buildLogMessage = "Building source and wheel archives...";
+        }
 
-	getLog().info(buildLogMessage);
-	poetryHelper.executeAndLogOutput(Arrays.asList(buildCommand));
+        getLog().info(buildLogMessage);
+        poetryHelper.executeAndLogOutput(Arrays.asList(buildCommand));
 
-	if (exportRequirementsFile) {
-	    getLog().info("Exporting requirements.txt file...");
+        if (exportRequirementsFile) {
+            getLog().info("Exporting requirements.txt file...");
 
-	    String outputFile = exportRequirementsFolder + "/requirements.txt";
-	    File directory = new File(exportRequirementsFolder);
-	    if (!directory.exists()) {
-		directory.mkdir();
-	    }
+            String outputFile = exportRequirementsFolder + "/requirements.txt";
+            File directory = new File(exportRequirementsFolder);
+            if (!directory.exists()) {
+                directory.mkdir();
+            }
 
-	    List<String> command = new ArrayList<>();
-	    command.add("export");
-	    command.add("--output");
-	    command.add(outputFile);
+            List<String> command = new ArrayList<>();
+            command.add("export");
+            command.add("--output");
+            command.add(outputFile);
 
-	    if (!exportRequirementsWithHashes) {
-		command.add("--without-hashes");
-	    }
+            if (!exportRequirementsWithHashes) {
+                command.add("--without-hashes");
+            }
 
-	    if (!exportRequirementsWithUrls) {
-		command.add("--without-urls");
-	    }
-	    poetryHelper.executeAndLogOutput(command);
-	}
+            if (!exportRequirementsWithUrls) {
+                command.add("--without-urls");
+            }
+            poetryHelper.executeAndLogOutput(command);
+
+            setUpPlaceholderFileAsMavenArtifact();
+        }
+    }
+
+    protected void setUpPlaceholderFileAsMavenArtifact() {
+        mavenArtifactFile.getParentFile().mkdirs();
+        try (PrintWriter writer = new PrintWriter(mavenArtifactFile)) {
+            writer.println("This is NOT the file you are looking for!");
+            writer.println();
+            writer.println("To take advantage of the Maven Reactor, we want to publish pom files for this artifact.");
+            writer.println("But Maven isn't the right solution for managing Python dependencies.");
+            writer.println();
+            writer.println(String.format("Please check your appropriate Python repository for the %s files instead!",
+                    project.getArtifactId()));
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Could not create placeholder artifact file!", e);
+        }
+
+        project.getArtifact().setFile(mavenArtifactFile);
     }
 
 }
