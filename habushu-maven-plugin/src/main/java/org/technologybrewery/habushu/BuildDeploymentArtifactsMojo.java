9642ea18dfcb26006327870696890da264993908
Jeff Ross <849346@bah.com>
2024-01-05 09:40:44 -0500

    HAB-77 - Moving cacheWheels config implementation from PACKAGE phase to INSTALL phase

--- habushu-maven-plugin/src/main/java/org/technologybrewery/habushu/BuildDeploymentArtifactsMojo.java
+++ habushu-maven-plugin/src/main/java/org/technologybrewery/habushu/BuildDeploymentArtifactsMojo.java
@@ -127,10 +127,6 @@ public class BuildDeploymentArtifactsMojo extends AbstractHabushuMojo {
 
             setUpPlaceholderFileAsMavenArtifact();
         }
-
-        if(cacheWheels){
-            cacheWheelFile();
-        }
     }
 
     private void logLocalMonorepoCaveats() {
@@ -166,27 +162,5 @@ public class BuildDeploymentArtifactsMojo extends AbstractHabushuMojo {
         }
 
         project.getArtifact().setFile(mavenArtifactFile);
-    }
-
-    private void cacheWheelFile() {
-        PoetryCommandHelper poetryHelper = createPoetryCommandHelper();
-        try{
-            File wheelSourceDirectory = new File(project.getBuild().getDirectory());
-            String poetryCacheDirectoryPath = poetryHelper.getPoetryCacheDirectoryPath();
-            File poetryWheelCacheDirectory = new File(String.format("%s/cache/repositories/wheels/%s", poetryCacheDirectoryPath, project.getArtifactId()));
-            //conditional will throw an error if cache directory isn't created 
-            if(poetryWheelCacheDirectory.exists() || poetryWheelCacheDirectory.mkdirs()){
-                List<File> wheelFiles = Stream.of(wheelSourceDirectory.listFiles())
-                                              .filter(file -> file.getAbsolutePath().endsWith(".whl"))
-                                              .map(File::getAbsoluteFile)
-                                              .collect(Collectors.toList());
-                for(File file : wheelFiles){
-                    HabushuUtil.copyFile(file.getPath(), String.format("%s/%s", poetryWheelCacheDirectory.toPath().toString(), file.getName()));
-                    getLog().info(String.format("Cached the %s file", file.getName()));
-                }
-            }
-        } catch (Exception e){
-            throw new HabushuException("Could not cache the " + project.getArtifactId() + " wheel file(s)!", e);
-        }
-    }    
+    }  
 }
