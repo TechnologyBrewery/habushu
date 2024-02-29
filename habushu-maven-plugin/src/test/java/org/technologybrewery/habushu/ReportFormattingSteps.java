f8abe01d8b91a0c706a2c0398837e72865712edb
Isaac Becerra-Llanos <becerrallanos_isaac@ne.bah.com>
2024-02-29 10:48:14 -0500

    Expand comments

--- habushu-maven-plugin/src/test/java/org/technologybrewery/habushu/ReportFormattingSteps.java
+++ habushu-maven-plugin/src/test/java/org/technologybrewery/habushu/ReportFormattingSteps.java
@@ -6,11 +6,11 @@ import io.cucumber.java.en.When;
 public class ReportFormattingSteps {
     @When("the Habushu project is built")
     public void the_habushu_project_is_built() {
-        // Manual
+        // Manual - test is dependent on files generated after Behave runs and cannot be automated
     }
 
     @Then("a Cucumber report file {string} exists")
     public void a_cucumber_report_file_exists(String string) {
-        // Manual
+        // Manual - test is dependent on files generated after Behave runs and cannot be automated
     }
 }
