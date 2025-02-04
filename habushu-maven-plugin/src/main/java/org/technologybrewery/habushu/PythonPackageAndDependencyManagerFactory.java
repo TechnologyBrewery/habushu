638bed25ec7291b12b29622fda8a00ce5699f531
Margaret Black <black_margaret@bah.com>
2025-02-04 13:20:49 -0600

    Refactored PyenvAndPoetrySetup such that PythonPackageAndDependencyManagerSetup can select between poetry or uv; uv functionality tbd

---
+++ habushu-maven-plugin/src/main/java/org/technologybrewery/habushu/PythonPackageAndDependencyManagerFactory.java
@@ -0,0 +1,22 @@
+package org.technologybrewery.habushu;
+
+import java.io.File;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
+import org.technologybrewery.habushu.util.HabushuUtil;
+
+public class PythonPackageAndDependencyManagerFactory {
+    public static AbstractPythonPackageAndDependencyManagerSetup createPythonPackageAndDependencyManagerSetup(
+        String pythonVersion, File baseDir, boolean rewriteLocalPathDepsInArchives, Log log, String pythonPackageAndDependencyManager,
+        Boolean usePyenv, File patchInstallScript) throws MojoExecutionException {
+        if (pythonPackageAndDependencyManager.equals(HabushuUtil.DEFAULT_PYTHON_PACKAGE_AND_DEPENDENCY_MANAGER)){
+            if ((usePyenv == null) || (patchInstallScript == null)) {
+                throw new MojoExecutionException("PyenvAndPoetrySetup requires usePyenv and patchInstallScript.");
+            }
+            return new PyenvAndPoetrySetup(pythonVersion, baseDir, rewriteLocalPathDepsInArchives, log, usePyenv, patchInstallScript);
+        } else {
+            // todo: return new UvSetup();
+            throw new MojoExecutionException("UvSetup not yet implemented"); 
+        }
+    }
+}
