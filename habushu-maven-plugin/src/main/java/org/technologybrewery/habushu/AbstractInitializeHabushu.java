6f8a75f50db4f303af00f9bdd4cfbd15ebbe904c
Choi, James [USA] <Choi_James@bah.com>
2025-02-13 13:40:31 -0500

    217-uv-poetry-initializehabushuMOJO

---
+++ habushu-maven-plugin/src/main/java/org/technologybrewery/habushu/AbstractInitializeHabushu.java
@@ -0,0 +1,53 @@
+package org.technologybrewery.habushu;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+
+import java.io.File;
+
+/**
+ * Abstract class that ensures pre-requisite tools that Habushu leverages are installed and available on the
+ * developer's machine to support the same functionality across multiple Mojo implementations.
+ */
+public abstract class AbstractInitializeHabushu {
+    /**
+     * Base directory from which to write Python package and dependency management files.
+     */
+    protected File baseDir;
+
+    /**
+     * Logger from calling class to leverage.
+     */
+    protected Log log;
+
+    /**
+     * Boolean to check whether to override package version
+     */
+    protected boolean overridePackageVersion;
+
+    /**
+     * Expected PackageVersion from pom file.
+     */
+    protected String expectedPythonPackageVersion;
+
+
+    /**
+     * New instance - these values are typically passed in from Maven-enabled parameters in the calling Mojo.
+     *
+     * @param baseDir                        base directory from which to operate for this module
+     * @param log                            the logger to use for output
+     * @param overridePackageVersion         whether we override package version
+     * @param expectedPythonPackageVersion   expected PackageVersion from pom file
+     */
+    public AbstractInitializeHabushu(File baseDir,  Log log, boolean overridePackageVersion, String expectedPythonPackageVersion) {
+        this.baseDir = baseDir;
+        this.log = log;
+        this.overridePackageVersion = overridePackageVersion;
+        this.expectedPythonPackageVersion = expectedPythonPackageVersion;
+    }
+
+    public abstract void doExecute() throws MojoExecutionException, MojoFailureException;
+
+
+}
